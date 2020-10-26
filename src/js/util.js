8de756ed988fa9446d7edc7ed406752f3b6848e6
Sasha H <sashahdev23@gmail.com>
2020-10-26 19:58:45 +0200

    Create models (EventInfo, CallTree, Components) as data storages

--- src/js/util.js
+++ src/js/util.js
@@ -462,6 +462,39 @@ export function lazyPanToElement(viewport, element, padding = 0) {
   viewport.translateBy(x, y);
 }
 
+function nodeFullyVisible(viewport, node) {
+  if (!node) return false;
+  return Geometry.contains(
+    viewport.element.getBoundingClientRect(),
+    node.getBoundingClientRect(),
+  );
+}
+
+// Pan the scenario view to given HTMLElement node.
+export function panToNode(viewport, node) {
+  // To minimize panning do not move the view if the node is already fully visible.
+  if (!node || nodeFullyVisible(viewport, node)) {
+    return;
+  }
+
+  let target;
+  // If a node is already selected and visible, pan so that
+  // the new selection will be in the same place.
+  const highlightedNode = viewport.element.querySelector('.node.highlight');
+  if (nodeFullyVisible(viewport, highlightedNode)) {
+    const xform = d3.zoomTransform(highlightedNode);
+
+    // we'll have to offset for the border
+    const style = getComputedStyle(highlightedNode);
+    target = xform.apply([
+      highlightedNode.offsetLeft + Number.parseInt(style.borderLeftWidth, 10),
+      highlightedNode.offsetTop + Number.parseInt(style.borderTopWidth, 10),
+    ]);
+  }
+
+  viewport.translateTo(node.offsetLeft, node.offsetTop, target);
+}
+
 export {
   capitalizeString,
   color,
