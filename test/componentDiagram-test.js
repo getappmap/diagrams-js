1b54761042d80c4aeb001a29fde606dfedcdeaa1
Sasha H <sashahdev23@gmail.com>
2020-11-30 23:55:05 +0200

    Add simple Tape tests for Component diagram

--- test/componentDiagram-test.js
+++ test/componentDiagram-test.js
@@ -1,4 +1,4 @@
-import cdiagram from '../src/js/components/componentDiagram/index.js';
+import ComponentDiagram from '../src/js/components/componentDiagram/index.js';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -22,10 +22,37 @@ global.location = window.location;
 SVGElement.prototype.getBBox = () => ({x: 0, y: 0, width: 0, height: 0});
 SVGElement.prototype.getScreenCTM = () => (new DOMMatrix());
 
-test('should render a diagram', function (t) {
-  const model = readJSON('fullModel.json');
-  const elem = document.querySelector('#diagram');
-  const diagram = new cdiagram(elem);
-  diagram.render(model);
-  t.end();
+const model = readJSON('fullModel.json');
+const elem = document.querySelector('#diagram');
+const componentDiagram = new ComponentDiagram(elem);
+
+test('component diagram', (t) => {
+  t.test('should be rendered', function (t) {
+    componentDiagram.render(model);
+
+    t.equal(elem.querySelectorAll('.appmap__component-diagram').length, 1);
+    t.end();
+  });
+
+  t.test('should have 17 nodes', function (t) {
+    t.equal(elem.querySelectorAll('.nodes g.node').length, 17);
+    t.end();
+  });
+
+  t.test('node "HTTP" should be expanded', function (t) {
+    t.equal(elem.querySelector('.nodes .node[id="HTTP"]'), null);
+    t.equal(elem.querySelectorAll('.nodes .node[id="POST /applications"]').length, 1);
+    t.end();
+  });
+
+  t.test('node "SQL" should be highlighted', function (t) {
+    const node = elem.querySelector('.nodes .node[id="SQL"]');
+
+    t.notEqual(node, null);
+
+    node.dispatchEvent(new window.Event('click'));
+
+    t.equal(node.classList.contains('highlight'), true);
+    t.end();
+  });
 });
